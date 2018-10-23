66aaa1d131a71d79de4e7b7b00b5e77cf08a62fd
straw <1107665984@qq.com>
2018-10-23 16:10:27 +0800

    dev fix

--- core/src/main/java/com/straw/maker/Application.java
+++ core/src/main/java/com/straw/maker/Application.java
@@ -4,6 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 
+/**
+ * 项目dev1.0
+ */
 @SpringBootApplication
 public class Application {
     public static void main(String[] args) {
